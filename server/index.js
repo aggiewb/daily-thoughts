3d870c08fbe31dc7e5c82aac75c73c15a84564bb
AggieWB <usmaggie23@gmail.com>
2021-02-04 09:37:34 -0800

    Add connection to postgres database with pgpromise
    Add call to connect to check database connection

--- server/index.js
+++ server/index.js
@@ -6,6 +6,28 @@ app.listen(port, () => {
     console.log(`server listening at port ${port}`);
 });
 
+// https://github.com/vitaly-t/pg-promise
+const pgPromise = require('pg-promise')();
+require('dotenv').config();
+const connectionObj = {
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: 'DailyThoughts',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    max: 20
+};
+const db = pgPromise(connectionObj);
+db.connect()
+    .then(obj => {
+        const databaseVersion = obj.client.serverVersion;
+        console.log(`SUCCESS, Database version: ${databaseVersion}`);
+        obj.done(); // success, release the connection
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+});
+
 app.use((request, response) => {
     response.type('text/html');
     response.status(404);
